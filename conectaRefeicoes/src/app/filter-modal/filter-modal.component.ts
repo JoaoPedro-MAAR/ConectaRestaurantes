2185d34127b32d759fb575620629a3e53851854e
JoaoPedro-MAAR <2005joaopedromaar@gmail.com>
2025-06-14 19:43:56 -0300

    Implement FilterModal component with basic structure and styles; update ListOrder component to display the last three orders; enhance types with status for Solicitation.

---
+++ conectaRefeicoes/src/app/filter-modal/filter-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms'
+
+@Component({
+  selector: 'app-filter-modal',
+  imports: [ReactiveFormsModule],
+  templateUrl: './filter-modal.component.html',
+  styleUrl: './filter-modal.component.css'
+})
+export class FilterModalComponent {
+
+
+  
+}
--- conectaRefeicoes/src/types.ts
+++ conectaRefeicoes/src/types.ts
@@ -1,3 +1,8 @@
+
+type StatusSolicitation = "Recebido" | "Cancelado" | "Em preparo" | "Enviado" | "Finalizado"
+
+
+
 interface Pedido {
     id: number
     matricula: number;
@@ -8,10 +13,10 @@ interface Solicitation {
     id: number; 
     obra: string;
     gestor: string;
+    status: StatusSolicitation; 
     qtd_Marmitas: number;
     pedidos: Pedido[];
 }
-
 
 
 export type { Pedido, Solicitation }
