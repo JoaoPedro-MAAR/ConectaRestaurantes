dd609fb756f08b4ed09a2eb376a756674e50de9a
Andrey Coutinho <148481213+Androka2004@users.noreply.github.com>
2025-06-18 17:53:59 +0000

    feat: criado aviso de envio com sucesso do formulário

--- conectaRefeicoes/src/app/form-component/form-component.component.ts
+++ conectaRefeicoes/src/app/form-component/form-component.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms'
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 import { RequisicaoService } from '../services/requisicao.service';
 import { FormPedido } from '../model/pedido.interface';
 
@@ -15,20 +15,29 @@ export class FormComponentComponent {
 
   private refeicaoFormBuilder = inject(FormBuilder)
   private requisicaoService = inject(RequisicaoService)
-  
+  enviado = signal(false);
   refeicaoForm = this.refeicaoFormBuilder.group({
-      obra: [""],
-      gestor: [""],
-      qtd_Marmitas: [0],
+      obra: ["", Validators.required, Validators.minLength(3)],
+      gestor: ["", Validators.required, Validators.minLength(3)],
+      qtd_Marmitas: [0, [Validators.required, Validators.min(1)]],
       status:["Recebido"]
   })
 
 
   handleSubmit() {
 
+    if (this.refeicaoForm.invalid) {
+      this.refeicaoForm.markAllAsTouched();
+      return;
+    }
+
 
     const pedido = this.refeicaoForm.value;
     this.requisicaoService.postOrder(pedido as FormPedido);
-
+    this.enviado.set(true);
+    this.refeicaoForm.reset();
+    setTimeout(() => {
+      this.enviado.set(false);
+    }, 5000);
   }
 };
