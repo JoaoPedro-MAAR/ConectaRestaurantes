e0195c1f257aecfc31e0e5909626f8f49c89f315
Androka2004 <dev.andrey2004@gmail.com>
2025-12-14 17:30:57 -0300

    add menu filter modal component with template and styles

---
+++ src/app/menu-list-page/menu-filter-modal/menu-filter-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-filter-modal',
+  imports: [],
+  templateUrl: './menu-filter-modal.component.html',
+  styleUrl: './menu-filter-modal.component.css'
+})
+export class MenuFilterModalComponent {
+
+}
